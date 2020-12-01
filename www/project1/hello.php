8ecb930eb079c327590705fd4984935668663866
derio123 <deriomatheus6@gmail.com>
2020-12-01 20:35:30 -0300

    Iniciando projeto

---
+++ www/project1/hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FS-PHP</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+    <?php 
+        /**
+         * Bloco de saida da variavel start
+         * e comando de saida
+         */
+        $start = "Vamos lá";
+        echo "<h1>Olá mundo: {$start}</h1>";
+        echo "<p id='js' >Loading...</p>";
+    ?>
+
+    <script src="assets/script.js"></script>
+</body>
+</html>
